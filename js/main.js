ef4378b83baf4c3dc750f5db7f7f405b5193546c
EraliTemirov <eralitemirov129@gmail.com>
2024-12-14 19:24:33 +0500

    home page add

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const translations = {
+   en: {
+     home: "Home",
+     resheniya: "Solutions",
+     expertiza: "Expertise",
+     kontakt: "Contact",
+     text_hero:"By making the complex simple, we create your success"
+   },
+   ru: {
+     home: "Главная",
+     resheniya: "Решения",
+     expertiza: "Экспертиза",
+     kontakt: "Контакт",
+     text_hero:"Делая сложное простым, мы создаем ваш успех",
+   },
+   uz: {
+     home: "Bosh sahifa",
+     resheniya: "Yechimlar",
+     expertiza: "Ekspertiza",
+     kontakt: "Kontakt",
+     text_hero: "Kompleksni sodda qilib, biz sizning muvaffaqiyatingizni yaratamiz.",
+   },
+ };
+ 
+ const changeLanguage = (lang) => {
+   document.querySelectorAll("[data-key]").forEach((el) => {
+     const key = el.getAttribute("data-key");
+     el.textContent = translations[lang][key];
+   });
+ 
+   const navLinks = document.querySelectorAll(".navbar li a");
+   navLinks[0].textContent = translations[lang].home;
+   navLinks[1].textContent = translations[lang].resheniya;
+   navLinks[2].textContent = translations[lang].expertiza;
+   navLinks[3].textContent = translations[lang].kontakt;
+
+   const hero = document.querySelector(".hero-text");
+   hero.textContent = translations[lang].text_hero;
+ };
+ 
+ document.querySelectorAll(".dropdown-menu a").forEach((el) => {
+   el.addEventListener("click", (e) => {
+     e.preventDefault();
+     const lang = e.target.getAttribute("data-lang");
+     changeLanguage(lang);
+   });
+ });
+ 
